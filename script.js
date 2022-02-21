176c67edf6b98535f1ca9826dd4565bfe1761011
j-jinjia <j.jinjia99@gmail.com>
2022-02-22 00:33:37 +0100

    Adding psuedo code

---
+++ script.js
@@ -0,0 +1,22 @@
+//global variables
+//DOM elements
+const paragraphMode = document.querySelector(".gameMode1");
+const randomWordsMode = document.querySelector(".gameMode2");
+const inputBar = document.querySelector(".inputBar")
+//functions
+/**A function that clears the introduction header, the instructions and the buttons when either button is clicked and displays the screen and input bigger. Also add a countdown before the game starts, score and timer. */
+/**A function that checks whether the input text matches the word displayed.
+ * if the word doesn't match : show red colour in input and word screen.
+ * if the word matches: show green colour in input and word screen.
+*/
+/**A function that starts after enter button is clicked
+ */
+onRandomWordButtonClick = () => {
+
+}
+
+onParagraphButtonClick =() => {
+}
+//Event listeners
+paragraphMode.addEventListener("click", onParagraphButtonClick);
+randomWordsMode.addEventListener("click", onRandomWordButtonClick);
